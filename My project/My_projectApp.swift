0c4091c61df1d914a10e2a850035f8425a96a7b3
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-12-29 18:43:55 +0200

    Initial Commit

---
+++ My project/My_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_projectApp.swift
+//  My project
+//
+//  Created by admin on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
